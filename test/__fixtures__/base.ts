e82cdd1dd2efc101a3fd303a9c4cf560f1e1d923
qwq <qwq@qwq.cat>
2023-08-03 13:25:42 +0800

    test: update snapshots

--- test/__fixtures__/base.ts
+++ test/__fixtures__/base.ts
@@ -1,13 +1,17 @@
 import { join } from 'path';
 
 export const obj = {
+  /*start*/
   a: '',
   c: {},
   b() {
     return 'b';
   },
 };
-export const arr = [];
+export const arr = [
+  //start
+  1, 2, 3,
+];
 switch ('') {
   case '':
     break;
@@ -22,12 +26,24 @@ const qwq = {
 let owo = 'owo';
 var { c, a, b } = obj;
 'abc'.replace(/a(?<foo>b)c/, '$1');
+//NOTE
 class Foo {
   b = 'bar';
-
+  d() {}
+  z() {}
   c() {}
-
   constructor() {}
-
   static a() {}
 }
+/* REVIEW*/
+interface ExampleObject {
+  '0'?: any;
+  z: number;
+  a?: any;
+  b?: any;
+  exampleNestedObject: ExampleNestedObject;
+}
+type ExampleNestedObject = {
+  z?: any;
+  a?: any;
+};
